b4f58b42170afd67fb72f7375f4543e86f9348f8
DaraSeam <ouk.sovannratana19@kit.edu.kh>
2020-10-09 14:56:48 +0700

    updated

--- src/4_Function/3_recursion.kt
+++ src/4_Function/3_recursion.kt
@@ -13,6 +13,7 @@ fun fact(n: Int): Long{
 }
 fun main (args: Array<String>){
     println("Factorial of 4 is: "+fact(5))
+    println("Hello World")
 }
 
 //first step:
@@ -20,7 +21,6 @@ fun main (args: Array<String>){
 //f(3)= 3*f(2)
 //f(2)= 2*f(1)
 //f(1)= 1 according to condition
-
 //second step time to evaluate:
 //f(2) = 2*1 = 2
 //f(3) = 3*2 = 6
